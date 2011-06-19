1bd4401cc7eff7f01cdc264b49c99910d72433b5
Michael Camilleri <camillerimichael@gmail.com>
2011-06-19 22:02:59 +0100

    redirects /index.html to home

--- config/routes.rb
+++ config/routes.rb
@@ -38,6 +38,7 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
   map.root :controller => "home"
+  map.connect '/index.html', :controller => 'home'
 
   # See how all your routes lay out with "rake routes"
 
