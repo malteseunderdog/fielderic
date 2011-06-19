dff87e502e2df91c2133fe2c4e970f1abbcd9ed2
Michael Camilleri <camillerimichael@gmail.com>
2011-06-19 21:22:45 +0100

    handles errors when IP location is not found

--- lib/player_locator.rb
+++ lib/player_locator.rb
@@ -1,8 +1,24 @@
 require 'rubygems'
 require 'geoip'
 class PlayerLocator  
-  def get_location(ip_address)
-    city_details = GeoIP.new('../resources/GeoLiteCity.dat').city(ip_address)
-    city_details['longitude'].to_s + "," + city_details['latitude'].to_s    
+  
+  def get_coordinates(ip_address)
+    begin
+      city_details = GeoIP.new('resources/GeoLiteCity.dat').city(ip_address)
+      city_details['longitude'].to_s + "," + city_details['latitude'].to_s
+    rescue
+      puts 'Error: Could not locate IP address ' + ip_address
+    end    
+    city_details = ""
   end    
+  
+  def get_location(ip_address)
+    begin
+      city_details = GeoIP.new('resources/GeoLiteCity.dat').city(ip_address)
+      city_details['city_name'] + ", " + city_details['country_name']
+    rescue
+      puts 'Error: Could not locate IP address ' + ip_address
+    end
+    city_details = ""
+  end
 end
