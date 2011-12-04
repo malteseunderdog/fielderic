3e4d2c473aff4003d4aaf4e48268e35fdf664042
Michael Camilleri <camillerimichael@gmail.com>
2011-12-04 00:36:28 +0000

    Added login to header

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,35 @@
-class HomeController < ApplicationController
+class HomeController < ApplicationController  
   def index
+    if params[:password].nil?
+      unless params[:nickname].nil?
+        @player = Player.get_player(params[:nickname])
+        unless @player.nil?
+          respond_to do |format|
+            format.html # show.html.erb
+            format.xml  { render :xml => @player }
+          end
+        else
+          @player = "Nickname error"
+          return @player 
+        end  
+      end
+    else
+      unless params[:nickname].nil?
+        @player = Player.get_player_with_password(params[:nickname], params[:password])
+        unless @player.nil?
+          respond_to do |format|
+            format.html # show.html.erb
+            format.xml  { render :xml => @player }
+          end
+        else
+          @player = params[:nickname]
+          return @player 
+        end  
+      end
+    end
   end
-
+  
+  def sign_out
+    session[:player]=nil
+  end
 end
