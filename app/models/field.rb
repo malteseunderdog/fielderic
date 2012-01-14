2212e1868180d45758efa97906c76e4ececca4e6
Michael Camilleri <camillerimichael@gmail.com>
2012-01-14 18:26:51 +0000

    organiser only can edit/delete matches

--- app/models/field.rb
+++ app/models/field.rb
@@ -4,4 +4,7 @@ class Field < ActiveRecord::Base
   def self.get_by_player(player_id)
     find(:first, :conditions => "field.player_id  = '#{player_id}'")
   end
+  def self.get_by_player_and_match(player_id, match_id)
+    find(:first, :conditions => "field.player_id = '#{player_id}' AND field.match_id = '#{match_id}'")
+  end 
 end
