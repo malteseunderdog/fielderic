0603c177ef080d77886ef0ca317fec0b4b1bb79c
JP <jp@javaclass.co.uk>
2012-01-14 01:54:38 +0000

    fixed deprecation warning (and web page icon)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,10 @@ namespace :db do
   task :seed => :environment do
     require 'active_record/fixtures'
     
-    # this order is important...
+    # this order is important...because of foreign keys
     Fixtures.create_fixtures('db/fixtures', 'player')
     Fixtures.create_fixtures('db/fixtures', 'match')
     Fixtures.create_fixtures('db/fixtures', 'field')
     
-    # Dir.glob(RAILS_ROOT + '/db/fixtures/*.yml').each do |file|
-      # # show some silly message
-      # puts "Loading fixture ... " + file 
-      # # load them
-      # Fixtures.create_fixtures('db/fixtures', File.basename(file, '.*'))
-    # end
   end
 end
