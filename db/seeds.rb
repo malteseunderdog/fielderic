582913fcbbe7421f8f3f0f6ebd78d350a41b15fb
JP <jp@javaclass.co.uk>
2012-01-14 01:11:20 +0000

    fixed seed ordering... failing because of foreign key

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,17 @@ namespace :db do
   desc "Load seed fixtures (from db/fixtures) into the current environment's database." 
   task :seed => :environment do
     require 'active_record/fixtures'
-    Dir.glob(RAILS_ROOT + '/db/fixtures/*.yml').each do |file|
-      # show some silly message
-      puts "Loading fixture ... " + file 
-      # load them
-      Fixtures.create_fixtures('db/fixtures', File.basename(file, '.*'))
-    end
+    
+    # this order is important...
+    Fixtures.create_fixtures('db/fixtures', 'player')
+    Fixtures.create_fixtures('db/fixtures', 'match')
+    Fixtures.create_fixtures('db/fixtures', 'field')
+    
+    # Dir.glob(RAILS_ROOT + '/db/fixtures/*.yml').each do |file|
+      # # show some silly message
+      # puts "Loading fixture ... " + file 
+      # # load them
+      # Fixtures.create_fixtures('db/fixtures', File.basename(file, '.*'))
+    # end
   end
 end
