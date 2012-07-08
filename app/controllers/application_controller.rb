3dd543b7d431096184baa09033f12a26e121c827
malteseunderdog <jp@javaclass.co.uk>
2012-07-08 17:26:32 +0100

    new AJAX login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,7 @@ class ApplicationController < ActionController::Base
 
   before_filter :ensure_authenticated
   def ensure_authenticated
-    if current_facebook_user.nil?
-      #redirect_to url_for(:controller=>"home")
-      else
+    if !current_facebook_user.nil?
       player = Player.get_player_with_facebook_id(current_facebook_user.id)
       current_facebook_user.fetch
       if player == nil
@@ -29,6 +27,60 @@ class ApplicationController < ActionController::Base
     # User is simply signed out of FE - no need to do any handling here.
     end
   end
+  
+  
+  def fb_connect_async_js(app_id=Facebooker2.app_id,options={},&proc)
+    puts "il madonna putz man! u ta hdejja!"
+    opts = Hash.new.merge!(options)
+    cookie = opts[:cookie].nil? ? true : opts[:cookie]
+    status = opts[:status].nil? ? true : opts[:status]
+    xfbml = opts[:xfbml].nil?   ? true : opts[:xfbml]
+    channel_url = opts[:channel_url]
+    lang = opts[:locale] || 'en_US'
+    extra_js = capture(&proc) if block_given?
+    js = <<-JAVASCRIPT
+    <div id="fb-root"></div>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId  : '#{app_id}',
+          status : #{status}, // check login status
+          cookie : #{cookie}, // enable cookies to allow the server to access the session
+          #{"channelUrl : '#{channel_url}', // add channelURL to avoid IE redirect problems" unless channel_url.blank?}
+          oauth : true,
+          xfbml  : #{xfbml}  // parse XFBML
+        });
+        #{extra_js}
+        
+        FB.getLoginStatus(function(response){
+          alert(response.authResponse.userID);
+          FB.api('/me', function(responseForName) {
+            alert('Your name is ' + responseForName.username);
+            $("#fb_username").html(responseForName.username);
+          });
+        });
+        
+      };
+
+      (function() {
+        var e = document.createElement('script'); e.async = true;
+        e.src = document.location.protocol + '//connect.facebook.net/#{lang}/all.js';
+        document.getElementById('fb-root').appendChild(e);
+      }());
+    </script>
+    JAVASCRIPT
+    escaped_js = fb_html_safe(js)
+    if block_given? 
+     concat(escaped_js)
+     #return the empty string, since concat returns the buffer and we don't want double output
+     # from klochner
+     "" 
+    else
+     escaped_js
+    end
+  end
+  
+  
 
   def current_player
     @current_player ||= Player.find(session[:player_id]) if session[:player_id]
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,66 +1,46 @@
 # This controller handles the login/logout function of the site.
 class SessionsController < ApplicationController
-  # render new.erb.html
-  def new
-  end
 
-  def create    
-    session[:email] = params[:email]    
-    player = Player.get_player(params[:email])
+  # method called upon clicking Play button
+  def login
+    email = params[:email]
+    password = params[:password]
+        
+    player = Player.get_player(email)
     if (!player.nil?)
-      password = player.password
-      needs_password = (!password.nil? && !password.empty?)
-      is_password_provided = (!params[:password].nil? && !params[:password].empty?)
-      if (needs_password && !is_password_provided)
-        # User account has a password set but no password was given at login
-        session[:needs_password] = true
-        redirect_to "/"
-      else
-        if (is_password_provided)
-          player = Player.get_player_with_password(params[:email], params[:password])
-        else
-          player = Player.get_player(params[:email])
-        end
-        if player
-          # Protects against session fixation attacks, causes request forgery
-          # protection if user resubmits an earlier form using back
-          # button. Uncomment if you understand the tradeoffs.
-          # reset_session
+      # let us check if we should show a password field or not
+      @needs_password_field = !player.password.blank?
+
+      if @needs_password_field
+        # user needs a password -- but we might have already set this and we want
+        # to check the password is correct
+        if password != player.password and !password.blank? 
+          flash.now[:error] = "Incorrect username and password combination"
+        elsif !password.nil? and password.empty?
+          flash.now[:error] = "password empty"
+        elsif password == player.password
           session[:logged_in_player] = player
-          new_cookie_flag = (params[:remember_me] == "1")
-          redirect_to "/"
-          flash[:notice] = "Logged in successfully"
-        else
-          # Incorrect email/password pair
-          fail_login
-        end
-      end
+        end        
+      else
+        # no need for a password...
+        session[:logged_in_player] = player
+      end 
+      
     else
-      # Email not found
-      fail_login  
-    end   
-  end
-  
-  def fail_login
-    note_failed_signin
-    @email    = params[:email]
-    @remember_me = params[:remember_me]
-    session[:needs_password] = false
-    redirect_to "/"
+      # use flesh.now as I want this only available for current request
+      flash.now[:error] = "Unknown user email"
+    end
+        
+    respond_to do |format|
+      format.js { render :layout=>false }     
+    end
   end
 
-  def destroy
+  # method called upon clicking Play button
+  def logout
     session[:logged_in_player] = nil
-    session[:needs_password] = false
-    flash[:notice] = "You have been logged out."
     redirect_to "/"
   end
 
-protected
-
-  # Track failed login attempts
-  def note_failed_signin
-    flash[:error] = "Couldn't log you in with '#{params[:email]}'"
-    logger.warn "Failed login for '#{params[:email]}' from #{request.remote_ip} at #{Time.now.utc}"
-  end
+    
 end
