ea0b498830eabbc46967bc9898bf13e0201a81ef
malteseunderdog <jp@javaclass.co.uk>
2013-03-24 21:28:52 +0000

    Now checking only after . after @ of email address

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -8,21 +8,28 @@
 $(document).ready(function() {
     $('#passwordStuff').hide(); // hide it
     $('#email').keyup(function() {
-        $.ajax({
-            type: "POST",
-            dataType: "html",
-            url:  "/password",
-            data: { email: $('#email').val() },
-            success: function (data) {
-                console.log(data); // <==============================
-                if (data == "true") {
-                    $('#passwordStuff').show();
-                } else {
-                    $('#passwordStuff').hide();
-                }
-                //alert(data);
+        var email = $('#email').val();
+        // only consider after @ sign - less http calls
+        var atIdx = email.indexOf("@");        
+        if (atIdx > -1) {
+            var domain = email.substring(atIdx);
+            if (domain.indexOf(".") > -1) {
+                $.ajax({
+                    type: "POST",
+                    dataType: "html",
+                    url:  "/password",
+                    data: { email: $('#email').val() },
+                    success: function (data) {
+                        //console.log(data); // <========= remove in production
+                        if (data == "true") {
+                            $('#passwordStuff').show();
+                        } else {
+                            $('#passwordStuff').hide();
+                        }
+                    }
+                });
             }
-        });
+        }
     });
 });
 
